278fa9ac1994371ded0f27b3d3dc4dbbe771ba3f
sachin <sachinkshuklaoo7@gamil.com>
2020-02-29 15:16:54 +0530

    Finished Project

--- lib/pages/character_detail_screen.dart
+++ lib/pages/character_detail_screen.dart
@@ -1,3 +1,4 @@
+import 'package:after_layout/after_layout.dart';
 import 'package:despicable_me_app/models/character.dart';
 import 'package:despicable_me_app/styleguide.dart';
 import 'package:flutter/material.dart';
@@ -5,13 +6,22 @@ import 'package:flutter/material.dart';
 class CharacterDetailScreen extends StatefulWidget {
   final Character character;
 
+  final double _expanedeBottomSheetBottomPosition = 0;
+  final double _collapsedBottomSheetBottomPosition = -250;
+  final double _completeCollapsedBottomSheetBottomPosition = -330;
+
+
   const CharacterDetailScreen({Key key, this.character}) : super(key: key);
 
   @override
   _CharacterDetailScreenState createState() => _CharacterDetailScreenState();
 }
 
-class _CharacterDetailScreenState extends State<CharacterDetailScreen> {
+class _CharacterDetailScreenState extends State<CharacterDetailScreen> with AfterLayoutMixin<CharacterDetailScreen>{
+
+  double _bottomSheetBottomPosition = -330;
+  bool isCollapsed = false;
+
   @override
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
@@ -44,14 +54,17 @@ class _CharacterDetailScreenState extends State<CharacterDetailScreen> {
                     icon: Icon(Icons.close),
                     color: Colors.white.withOpacity(0.9),
                     onPressed: () {
-                      Navigator.pop(context);
+                      Future.delayed(const Duration(milliseconds: 250), () {
+                        _bottomSheetBottomPosition = widget._completeCollapsedBottomSheetBottomPosition;
+                        Navigator.pop(context);
+                      });
                     },
                   ),
                 ),
                 Align(
                   alignment: Alignment.topRight,
                   child: Hero(
-                      tag: "image-{$characters[0].name}",
+                      tag: "image-${characters[0].name}",
                       child: Image.asset(widget.character.imagePath, height: screenHeight * 0.45)),
                 ),
                 Padding(
@@ -69,8 +82,116 @@ class _CharacterDetailScreenState extends State<CharacterDetailScreen> {
               ],
             ),
           ),
+          AnimatedPositioned(
+            duration: const Duration(milliseconds: 500),
+            curve: Curves.fastLinearToSlowEaseIn,
+            bottom: _bottomSheetBottomPosition,
+            right: 0,
+            left: 0,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(40),
+                  topRight: Radius.circular(40)
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  InkWell(
+                    onTap: _onTap,
+                    child: Container(
+                      alignment: Alignment.centerLeft,
+                      padding: const EdgeInsets.symmetric(horizontal: 32),
+                      height: 80,
+                      child: Text(
+                        'Clips',
+                        style: AppTheme.subHeading.copyWith(color: Colors.black),
+                      ),
+                    ),
+                  ),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: _clipsWidget(),
+                  )
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
+  }
+  Widget _clipsWidget() {
+    return Container(
+      height: 250,
+      margin: const EdgeInsets.symmetric(horizontal: 16),
+      child: Row(
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              roundedContainer(Colors.redAccent),
+              SizedBox(height: 20),
+              roundedContainer(Colors.greenAccent),
+            ],
+          ),
+          SizedBox(width: 16),
+          Column(
+            children: <Widget>[
+              roundedContainer(Colors.orangeAccent),
+              SizedBox(height: 20),
+              roundedContainer(Colors.purple),
+            ],
+          ),
+          SizedBox(width: 16),
+          Column(
+            children: <Widget>[
+              roundedContainer(Colors.grey),
+              SizedBox(height: 20),
+              roundedContainer(Colors.blueGrey),
+            ],
+          ),
+          SizedBox(width: 16),
+          Column(
+            children: <Widget>[
+              roundedContainer(Colors.lightGreenAccent),
+              SizedBox(height: 20),
+              roundedContainer(Colors.pinkAccent),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget roundedContainer(Color color) {
+    return Container(
+      width: 100,
+      height: 100,
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+      ),
+    );
+  }
+
+  _onTap() {
+    setState(() {
+      _bottomSheetBottomPosition = isCollapsed
+          ? widget._expanedeBottomSheetBottomPosition
+          : widget._collapsedBottomSheetBottomPosition;
+      isCollapsed = !isCollapsed;
+    });
+  }
+
+  @override
+  void afterFirstLayout(BuildContext context) {
+    Future.delayed(const Duration(milliseconds: 500), () {
+      setState(() {
+        isCollapsed = true;
+        _bottomSheetBottomPosition = widget._collapsedBottomSheetBottomPosition;
+      });
+    });
   }
 }
--- lib/widgets/character_widget.dart
+++ lib/widgets/character_widget.dart
@@ -4,6 +4,16 @@ import 'package:despicable_me_app/styleguide.dart';
 import 'package:flutter/material.dart';
 
 class CharacterWidget extends StatelessWidget {
+  final Character character;
+  final PageController pageController;
+  final int currentPage;
+
+  const CharacterWidget({
+    this.character,
+    this.pageController,
+    this.currentPage,
+  });
+
   @override
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
@@ -15,66 +25,77 @@ class CharacterWidget extends StatelessWidget {
             context,
             PageRouteBuilder(
                 transitionDuration: const Duration(milliseconds: 350),
-                pageBuilder: (context, _, __) => CharacterDetailScreen(character: characters[0])));
+                pageBuilder: (context, _, __) =>
+                    CharacterDetailScreen(character: character)));
       },
-      child: Stack(
-        children: [
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: ClipPath(
-              clipper: CharacterCardBackgroundClipper(),
-              child: Hero(
-                tag: "background-${characters[0].name}",
-                child: Container(
-                  height: 0.6 * screenHeight,
-                  width: 0.9 * screenWidth,
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: characters[0].colors,
-                      begin: Alignment.topRight,
-                      end: Alignment.bottomLeft,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
-          Align(
-            alignment: Alignment(0, -0.5),
-            child: Hero(
-              tag: "image-{$characters[0].name}",
-              child: Image.asset(
-                characters[0].imagePath,
-                height: screenHeight * 0.55,
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 48, right: 16, bottom: 16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                Hero(
-                  tag: "name-${characters[0].name}",
-                  child: Material(
-                    color: Colors.transparent,
+      child: AnimatedBuilder(
+        animation: pageController,
+        builder: (context, child){
+          double value = 1;
+          if(pageController.position.haveDimensions) {
+            value = pageController.page - currentPage;
+            value = (1 - (value.abs() * .6)).clamp(0, 1);
+          }
+          return Stack(
+            children: [
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: ClipPath(
+                  clipper: CharacterCardBackgroundClipper(),
+                  child: Hero(
+                    tag: "background-${character.name}",
                     child: Container(
-                      child: Text(
-                        characters[0].name,
-                        style: AppTheme.heading,
+                      height: 0.6 * screenHeight,
+                      width: 0.9 * screenWidth,
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: character.colors,
+                          begin: Alignment.topRight,
+                          end: Alignment.bottomLeft,
+                        ),
                       ),
                     ),
                   ),
                 ),
-                Text(
-                  "Tap to Read more",
-                  style: AppTheme.subHeading,
+              ),
+              Align(
+                alignment: Alignment(0, -0.5),
+                child: Hero(
+                  tag: "image-${character.name}",
+                  child: Image.asset(
+                    character.imagePath,
+                    height: screenHeight * 0.55 * value,
+                  ),
                 ),
-              ],
-            ),
-          ),
-        ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 48, right: 16, bottom: 16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Hero(
+                      tag: "name-${character.name}",
+                      child: Material(
+                        color: Colors.transparent,
+                        child: Container(
+                          child: Text(
+                            character.name,
+                            style: AppTheme.heading,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Text(
+                      "Tap to Read more",
+                      style: AppTheme.subHeading,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
@@ -88,15 +109,17 @@ class CharacterCardBackgroundClipper extends CustomClipper<Path> {
 
     clippedPath.moveTo(0, size.height * 0.4);
     clippedPath.lineTo(0, size.height - curveDistance);
-    clippedPath.quadraticBezierTo(1, size.height - 1, 0 + curveDistance, size.height);
+    clippedPath.quadraticBezierTo(
+        1, size.height - 1, 0 + curveDistance, size.height);
     clippedPath.lineTo(size.width - curveDistance, size.height);
-    clippedPath.quadraticBezierTo(
-        size.width + 1, size.height - 1, size.width, size.height - curveDistance);
+    clippedPath.quadraticBezierTo(size.width + 1, size.height - 1, size.width,
+        size.height - curveDistance);
     clippedPath.lineTo(size.width, 0 + curveDistance);
-    clippedPath.quadraticBezierTo(
-        size.width - 1, 0, size.width - curveDistance - 5, 0 + curveDistance / 3);
+    clippedPath.quadraticBezierTo(size.width - 1, 0,
+        size.width - curveDistance - 5, 0 + curveDistance / 3);
     clippedPath.lineTo(curveDistance, size.height * 0.29);
-    clippedPath.quadraticBezierTo(1, (size.height * 0.30) + 10, 0, size.height * 0.4);
+    clippedPath.quadraticBezierTo(
+        1, (size.height * 0.30) + 10, 0, size.height * 0.4);
     return clippedPath;
   }
 
